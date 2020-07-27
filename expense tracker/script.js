7b58afcd7d6386621d50ae81ae40e46607d44be1
laradamerji <67574533+laradamerji@users.noreply.github.com>
2020-07-27 09:35:20 +0300

    Add files via upload

---
+++ expense tracker/script.js
@@ -0,0 +1,82 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form= document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+//const dummyTransactions = []
+ //   { id: 1, text: 'Flower', amount: -20 },
+  //  { id: 1, text: 'Salary', amount: 300 },
+    //{ id: 1, text: 'Book', amount: -10 },
+    //{ id: 1, text: 'Camera', amount: 150 }
+//];
+
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions')); 
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+
+
+// Add transactions to DOM list
+function addtransactionDOM(transaction) {
+    // Get sign
+    const sign = transaction.amount < 0 ? '-' : '+'
+
+    const item = document.createElement('li');
+
+    //Add class based on value
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs
+        (transaction.amount)}</span> <button class="delete-btn">x</button>
+    `; 
+
+    list.appendChild(item)
+}
+
+// update the balance , income and expense 
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0 )
+    .toFixed(2);
+
+    const income = amounts
+     .filter(item => item > 0)
+     .reduce((acc, item) => (acc += item), 0)
+     .toFixed(2);
+
+cont expense = (
+    amount.filter(item => item < 0).reduce(acc, item) => (acc += item), 0) * -1
+    ).toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+    
+    
+}
+//Remove transaction by ID
+function RemoveTransaction(id) {
+    transactions = transactions.filter(transaction = transaction.id !== id)
+}
+
+
+// Update local storage transactions
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+
+}
+
+
+// Init app
+function init() {
+    list.innerHTML = '';
+
+    transactions.forEach(addtransactionDOM);
+    updateValues();
+}
+
+init();
